5744676d37f31c687ae2e8d35c1281f73fbfd9e4
Blastose <blastoisejay@gmail.com>
2023-03-17 18:58:33 -0700

    Fix: match non-whitespace characters for superscript

--- tests/superscript.test.ts
+++ tests/superscript.test.ts
@@ -3,10 +3,12 @@ import { converter } from '../src/index.js';
 
 describe('superscript', () => {
 	test('normal superscript', () => {
-		const htmlResult = converter('^superscript');
-		expect(htmlResult).toBe('<p><sup>superscript</sup></p>');
+		const htmlResult = converter('^superscript.');
+		expect(htmlResult).toBe('<p><sup>superscript.</sup></p>');
 	});
 
+	// New reddit md doesn't allow for nested superscripts but old reddit does
+	// The converter matches new reddit in this case
 	test('nested superscripts', () => {
 		const htmlResult = converter('^j^j');
 		expect(htmlResult).toBe('<p><sup>j</sup><sup>j</sup></p>');
@@ -18,6 +20,18 @@ describe('superscript', () => {
 		);
 		expect(htmlResult).toBe(
 			'<p>reddit <sup>and</sup> <a href="https://www.redditgifts.com"><sup>be</sup> <sup>reddited</sup></a> <sup>in</sup> <sup>return.</sup></p>'
+		);
+	});
+
+	test('superscripts with brackets', () => {
+		const htmlResult = converter('^(superscript is cool)');
+		expect(htmlResult).toBe('<p><sup>superscript is cool</sup></p>');
+	});
+
+	test('superscript with em', () => {
+		const htmlResult = converter('^(superscript-_+_++_+_++++___+_+__******#**!#@$#${}[])');
+		expect(htmlResult).toBe(
+			'<sup>superscript-<em>+</em>++<em>+</em>++++<strong><em>+</em>+</strong>***<em>**#</em>*!#@$#${}[]</sup>'
 		);
 	});
 });
