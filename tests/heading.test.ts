8edd63c36ab709f7c757ef3dc359fac1c5f56a88
Blastose <blastoisejay@gmail.com>
2023-03-18 20:27:00 -0700

    Add heading tests

--- tests/heading.test.ts
+++ tests/heading.test.ts
@@ -1,15 +1,16 @@
 import { describe, test, expect } from 'vitest';
 import { converter } from '../src/index.js';
 
-describe('general', () => {
-	test('bold-itatlic', () => {
-		const htmlResult = converter(
-			'It is ***error*** only, and not ___truth___, that shrinks from inquiry.'
-		);
+describe('heading', () => {
+	test('normal heading', () => {
+		const htmlResult = converter('#heading');
 
-		// em strong order is reversed in new reddit, but visually the result is the same
-		expect(htmlResult).toBe(
-			'<p>It is <em><strong>error</strong></em> only, and not <em><strong>truth</strong></em>, that shrinks from inquiry.</p>'
-		);
+		expect(htmlResult).toBe('<h1>heading</h1>');
+	});
+
+	test('underline heading', () => {
+		const htmlResult = converter('heading\n===');
+
+		expect(htmlResult).toBe('<h1>heading</h1>');
 	});
 });
