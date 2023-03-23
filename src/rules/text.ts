6709054b2e3b66b06dbe33e2762b0feb386a2d71
Blastose <blastoisejay@gmail.com>
2023-03-22 18:21:56 -0700

    Match `www.` for urls

--- src/rules/text.ts
+++ src/rules/text.ts
@@ -1,16 +1,18 @@
 import SimpleMarkdown from 'simple-markdown';
 import { SimpleMarkdownRule } from './ruleType.js';
+import { urlRegex } from './url.js';
 
 // Modifies original text rule to not output backslashes from urls
 // Also modifies the match function to not only capture text before `/`
 // This is used for matching redditlinks like `r/programing` so the `r`
 // is not captured before the rest of the link
+// Modifies match function to not capture text around www.
 export const text: SimpleMarkdownRule = Object.assign({}, SimpleMarkdown.defaultRules.text, {
 	match: SimpleMarkdown.anyScopeRegex(
-		/^[\s\S]+?(?=[^0-9A-Za-z\s\u00c0-\uffff]|\n\n| {2,}\n|\w+(?::|\/)\S|$)/
+		/^[\s\S]+?(?=[^0-9A-Za-z\s\u00c0-\uffff]|\n\n| {2,}\n|\w+(?::|\/|www.)|\S|$)/
 	) satisfies SimpleMarkdown.MatchFunction,
 	html: function (node) {
-		if (node.content.match(/^(https?:\/\/[^\s<]+[^<.,:;"')\]\s])/)) {
+		if (node.content.match(urlRegex)) {
 			node.content = node.content.replace(/\\/g, '');
 		}
 
--- src/rules/url.ts
+++ src/rules/url.ts
@@ -1,12 +1,15 @@
 import SimpleMarkdown from 'simple-markdown';
 import { SimpleMarkdownRule } from './ruleType.js';
 
+// Modifies original url regex to include www.
+export const urlRegex = /^((?:https?:\/\/|www\.)[^\s<]+[^<.,:;"')\]\s])/;
+
 // Modifies original url rule to ignore urls already parsed by the link rule
 export const url: SimpleMarkdownRule = Object.assign({}, SimpleMarkdown.defaultRules.url, {
 	match: function (source, state, prevCapture) {
 		if (state.link) {
 			return null;
 		}
-		return SimpleMarkdown.defaultRules.url.match(source, state, prevCapture);
+		return SimpleMarkdown.inlineRegex(urlRegex)(source, state, prevCapture);
 	} satisfies SimpleMarkdown.MatchFunction
 });
