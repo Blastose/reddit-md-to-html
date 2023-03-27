f3db6dce20b05c5754e91e5fab541cb669f8a30b
Blastose <blastoisejay@gmail.com>
2023-03-26 20:49:38 -0700

    Fix blockQuote match regex to match spaces after

--- src/rules/blockQuote.ts
+++ src/rules/blockQuote.ts
@@ -4,12 +4,13 @@ import { SimpleMarkdownRule } from './ruleType.js';
 // Modifies original blockQuote rule to not match >!, since >! !<
 // is used for spoiler text
 // Also modifies match regex to match empty text after `>`
+// and matches empty spaces after
 export const blockQuote: SimpleMarkdownRule = Object.assign(
 	{},
 	SimpleMarkdown.defaultRules.blockQuote,
 	{
 		match: SimpleMarkdown.blockRegex(
-			/^( {0,3}>(?!!).*?(?=\n\n|\n +\n))\n*/s
+			/^( {0,3}>(?!!).*?(?=\n\n|\n +\n))(?:\n| )*/s
 		) satisfies SimpleMarkdown.MatchFunction
 	}
 );
--- tests/blockquote.test.ts
+++ tests/blockquote.test.ts
@@ -22,8 +22,9 @@ Total mistakes found: 4265
 ^^PM ^^me ^^if ^^I'm ^^wrong ^^or ^^if ^^you ^^have ^^any ^^suggestions.   
 ^^[Github](https://github.com/chiefpat450119)`;
 		const htmlResult = converter(text);
-		expect(htmlResult).toBe(`<blockquote><ul><li>loosing custom</li></ul></blockquote><p>
-Did you mean to say &quot;losing&quot;?<br>Explanation: Loose is an adjective meaning the opposite of tight, while lose is a verb.<br>Total mistakes found: 4265<br><sup>I&#x27;m</sup> <sup>a</sup> <sup>bot</sup> <sup>that</sup> <sup>corrects</sup> <sup>grammar/spelling</sup> <sup>mistakes.</sup>
+
+		expect(htmlResult)
+			.toBe(`<blockquote><ul><li>loosing custom<br></li></ul></blockquote><p>Did you mean to say &quot;losing&quot;?<br>Explanation: Loose is an adjective meaning the opposite of tight, while lose is a verb.<br>Total mistakes found: 4265<br><sup>I&#x27;m</sup> <sup>a</sup> <sup>bot</sup> <sup>that</sup> <sup>corrects</sup> <sup>grammar/spelling</sup> <sup>mistakes.</sup>
 <sup>PM</sup> <sup>me</sup> <sup>if</sup> <sup>I&#x27;m</sup> <sup>wrong</sup> <sup>or</sup> <sup>if</sup> <sup>you</sup> <sup>have</sup> <sup>any</sup> <sup>suggestions.</sup><br><sup><a href="https://github.com/chiefpat450119" rel="noopener nofollow ugc">Github</a></sup></p>`);
 	});
 
@@ -49,6 +50,26 @@ This text should have a br before it`;
 			`<blockquote><p>This is some text
 This text should not be on a new line
 Nor should this text be<br>This text should have a br before it</p></blockquote>`
+		);
+	});
+
+	test('2 blockquotes separated by space', () => {
+		const text = `Really exciting seeing more updates from Bevy!
+
+Might be a dumb question but I really don't understand the \`in_set\` function from this post.
+
+>You can add systems to sets by calling the \`in_set\` method:
+ 
+>    app.add_system(gravity.in_set(PhysicsSet::Movement))
+
+Are we adding \`gravity\` to \`PhysicsSet\`? Why did we mention \`Movement\` then? Does this have to be added to a set inside a \`app.add_system()\` call? I feel like the other examples and docs don't help explain what is going on here...
+
+...
+
+Okay, I think I get it now, this isn't a set of systems, but instead a set of labels. I'm going to post my comment anyways because I personally found this very confusing, and I think the naming could use some work. Sounds amazingly powerful now that I understand it correctly.`;
+		const htmlResult = converter(text);
+		expect(htmlResult).toBe(
+			`<p>Really exciting seeing more updates from Bevy!</p><p>Might be a dumb question but I really don&#x27;t understand the <code>in_set</code> function from this post.</p><blockquote><p>You can add systems to sets by calling the <code>in_set</code> method:</p></blockquote><blockquote><p>app.add_system(gravity.in_set(PhysicsSet::Movement))</p></blockquote><p>Are we adding <code>gravity</code> to <code>PhysicsSet</code>? Why did we mention <code>Movement</code> then? Does this have to be added to a set inside a <code>app.add_system()</code> call? I feel like the other examples and docs don&#x27;t help explain what is going on here...</p><p>...</p><p>Okay, I think I get it now, this isn&#x27;t a set of systems, but instead a set of labels. I&#x27;m going to post my comment anyways because I personally found this very confusing, and I think the naming could use some work. Sounds amazingly powerful now that I understand it correctly.</p>`
 		);
 	});
 });
