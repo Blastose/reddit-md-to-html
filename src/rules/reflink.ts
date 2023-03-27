6e5bf4ec4951dea340365aaee8dcdacafe0ca8c7
Blastose <blastoisejay@gmail.com>
2023-03-27 16:36:34 -0700

    Add comment

--- src/rules/reflink.ts
+++ src/rules/reflink.ts
@@ -1,7 +1,8 @@
 import SimpleMarkdown from 'simple-markdown';
 import { SimpleMarkdownRule } from './ruleType.js';
 
-// Modifies original reflink rule to
+// Modifies original reflink rule to not match when a link is the second bracket
+// like [] []()
 export const reflink: SimpleMarkdownRule = Object.assign({}, SimpleMarkdown.defaultRules.reflink, {
 	match: function (source, state, prevCapture) {
 		const LINK_INSIDE = '(?:\\[[^\\]]*\\]|[^\\[\\]]|\\](?=[^\\[]*\\]))*';
