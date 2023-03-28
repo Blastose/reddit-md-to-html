b59ac8b757edae49084e778683ef9095149e3c4c
Blastose <blastoisejay@gmail.com>
2023-03-28 15:05:53 -0700

    Fix table html outputting more cols than cols in header

--- src/rules/table.ts
+++ src/rules/table.ts
@@ -3,8 +3,43 @@ import { SimpleMarkdownRule } from './ruleType.js';
 
 // Modifies original table rule to match loosely
 // The loose match does not require `|` to be at the start of a table row
+// Modifies html function to not output more columns than the number of header columns
 export const table: SimpleMarkdownRule = Object.assign({}, SimpleMarkdown.defaultRules.table, {
 	match: SimpleMarkdown.blockRegex(
 		/^ *(.+\|.*)\n *\|( *[-:]+[-| :]*)\n((?: *.*\|.*(?:\n|$))*)\n*/
-	) satisfies SimpleMarkdown.MatchFunction
+	) satisfies SimpleMarkdown.MatchFunction,
+	html: function (node, output, state) {
+		const getStyle = function (colIndex: number): string {
+			return node.align[colIndex] == null ? '' : 'text-align:' + node.align[colIndex] + ';';
+		};
+
+		const numColsInHeader = node.header.length;
+
+		const headers = node.header
+			.map(function (content: SimpleMarkdown.ASTNode, i: number) {
+				return SimpleMarkdown.htmlTag('th', output(content, state), {
+					style: getStyle(i),
+					scope: 'col'
+				});
+			})
+			.join('');
+
+		const rows = node.cells
+			.map(function (row: SimpleMarkdown.ASTNode) {
+				const rowLimitedCols = row.splice(0, numColsInHeader);
+				const cols = rowLimitedCols
+					.map(function (content: SimpleMarkdown.ASTNode, c: number) {
+						return SimpleMarkdown.htmlTag('td', output(content, state), { style: getStyle(c) });
+					})
+					.join('');
+
+				return SimpleMarkdown.htmlTag('tr', cols);
+			})
+			.join('');
+
+		const thead = SimpleMarkdown.htmlTag('thead', SimpleMarkdown.htmlTag('tr', headers));
+		const tbody = SimpleMarkdown.htmlTag('tbody', rows);
+
+		return SimpleMarkdown.htmlTag('table', thead + tbody);
+	} satisfies SimpleMarkdownRule['html']
 });
--- tests/table.test.ts
+++ tests/table.test.ts
@@ -41,6 +41,21 @@ describe('table', () => {
 		);
 	});
 
+	test('table with too many | after each row', () => {
+		const text = `|#|Match|PST|EST|CET|KST
+|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:|:--:
+|1|BDS vs AST|10:00 AM|1:00 PM|18:00|02:00||||
+|2|XL vs MAD|11:00 AM|2:00 PM|19:00|03:00||||
+|3|TH vs FNC|12:00 PM|3:00 PM|20:00|04:00||||
+|4|SK vs KOI|1:00 PM|4:00 PM|21:00|05:00||||
+|5|G2 vs VIT|2:00 PM|5:00 PM|22:00|06:00||||`;
+		const htmlResult = converter(text);
+
+		expect(htmlResult).toBe(
+			'<table><thead><tr><th style="text-align:center;" scope="col">#</th><th style="text-align:center;" scope="col">Match</th><th style="text-align:center;" scope="col">PST</th><th style="text-align:center;" scope="col">EST</th><th style="text-align:center;" scope="col">CET</th><th style="text-align:center;" scope="col">KST</th></tr></thead><tbody><tr><td style="text-align:center;">1</td><td style="text-align:center;">BDS vs AST</td><td style="text-align:center;">10:00 AM</td><td style="text-align:center;">1:00 PM</td><td style="text-align:center;">18:00</td><td style="text-align:center;">02:00</td></tr><tr><td style="text-align:center;">2</td><td style="text-align:center;">XL vs MAD</td><td style="text-align:center;">11:00 AM</td><td style="text-align:center;">2:00 PM</td><td style="text-align:center;">19:00</td><td style="text-align:center;">03:00</td></tr><tr><td style="text-align:center;">3</td><td style="text-align:center;">TH vs FNC</td><td style="text-align:center;">12:00 PM</td><td style="text-align:center;">3:00 PM</td><td style="text-align:center;">20:00</td><td style="text-align:center;">04:00</td></tr><tr><td style="text-align:center;">4</td><td style="text-align:center;">SK vs KOI</td><td style="text-align:center;">1:00 PM</td><td style="text-align:center;">4:00 PM</td><td style="text-align:center;">21:00</td><td style="text-align:center;">05:00</td></tr><tr><td style="text-align:center;">5</td><td style="text-align:center;">G2 vs VIT</td><td style="text-align:center;">2:00 PM</td><td style="text-align:center;">5:00 PM</td><td style="text-align:center;">22:00</td><td style="text-align:center;">06:00</td></tr></tbody></table>'
+		);
+	});
+
 	test('several tables with links in them', () => {
 		const text = `###[Event Overview](https://imgur.com/a/NbXKEjj)
 
