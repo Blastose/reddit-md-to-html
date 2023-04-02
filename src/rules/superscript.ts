087ab0f9df1a3b955f782e7fccbc650a5317adcd
Blastose <blastoisejay@gmail.com>
2023-04-02 15:35:01 -0700

    Fix superscript matching table separator (#10)

--- src/rules/superscript.ts
+++ src/rules/superscript.ts
@@ -3,7 +3,18 @@ import { SimpleMarkdownRule } from './ruleType.js';
 
 export const superscript: SimpleMarkdownRule = {
 	order: SimpleMarkdown.defaultRules.blockQuote.order - 0.5,
-	match: SimpleMarkdown.inlineRegex(/^\^+\(([^)]+)\)|^\^+([^\s^]+)/),
+	match: function (source, state, prevCapture) {
+		// If the superscript is in a table, we need to use a modified verision of the
+		// match regex to stop matching at `|` (table separator)
+		if (state.isTable) {
+			return SimpleMarkdown.inlineRegex(/^\^+\(([^)]+)\)|^\^+([^\s^|]+)/)(
+				source,
+				state,
+				prevCapture
+			);
+		}
+		return SimpleMarkdown.inlineRegex(/^\^+\(([^)]+)\)|^\^+([^\s^]+)/)(source, state, prevCapture);
+	},
 	parse: function (capture, parse, state) {
 		return {
 			content: parse(capture[1] ?? capture[2], state)
