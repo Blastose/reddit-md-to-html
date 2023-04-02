60a24c21540cce972903d82b2c0efbcd58cbd23e
Blastose <blastoisejay@gmail.com>
2023-04-02 11:13:05 -0700

    Fix list regex to match list items separated by 2+ new lines (#8)
    * Fix nested ol test text
    
    * Fix list ol not matching list items separated by new lines
    
    * WIP update list regex to match multiple new lines between li
    
    * Fix list regex
    
    * Update package

--- tests/orderedlist.test.ts
+++ tests/orderedlist.test.ts
@@ -35,19 +35,37 @@ describe('orderedlist', () => {
 			`<p>123. This should not turn into a ordered list</p><ol start="1"><li>This is a ol</li><li>this is also a ol</li></ol>`
 		);
 	});
+
+	test('list item separated by new line', () => {
+		const text = `Kotlin will always be compared to Java because 
+
+1. It was literally created to be a “better” Java. That’s also why JetBrains put so much effort in to supporting Java interoperability and compatibility with the same IDEs and tooling used by Java developers. 
+
+2. Kotlin’s power comes from java. Kotlin would be dead in the water if it wasn’t able to be compatible with the JVM and familiar enough where java developers could quickly onboard to Kotlin. Running on the same infrastructure as existing Java applications and being close enough to Java that existing developers can quickly onboard is what makes Kotlin an actually viable alternative to Java for companies.
+
+
+3. I love Kotlin.
+
+500 million. That's all folks.`;
+
+		const htmlResult = converter(text);
+		expect(htmlResult).toBe(
+			'<p>Kotlin will always be compared to Java because </p><ol start="1"><li><p>It was literally created to be a “better” Java. That’s also why JetBrains put so much effort in to supporting Java interoperability and compatibility with the same IDEs and tooling used by Java developers.</p></li><li><p>Kotlin’s power comes from java. Kotlin would be dead in the water if it wasn’t able to be compatible with the JVM and familiar enough where java developers could quickly onboard to Kotlin. Running on the same infrastructure as existing Java applications and being close enough to Java that existing developers can quickly onboard is what makes Kotlin an actually viable alternative to Java for companies.</p></li><li><p>I love Kotlin.</p></li></ol><p>500 million. That&#x27;s all folks.</p>'
+		);
+	});
 });
 
-// nested orderedlists do not work currently
-describe.skip('nested orderedlists', () => {
+describe('nested orderedlists', () => {
 	test('normal orderedlist', () => {
 		const text = `1. this
 2. is
-   1. a
-   2. great list
+  1. a
+  2. great list
 `;
 		const htmlResult = converter(text);
 		expect(htmlResult).toBe(
-			'<ol start="1"><li>this</li><li>is</li><li>a great</li><li>list</li><li>with</li><li>text</li></ol>'
+			`<ol start="1"><li>this</li><li>is
+<ol start="1"><li>a</li><li>great list</li></ol></li></ol>`
 		);
 	});
 });
