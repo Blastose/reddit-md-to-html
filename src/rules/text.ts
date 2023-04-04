49f548647e41c0a2d21d5466cb137f7cc524c09e
Blastose <blastoisejay@gmail.com>
2023-04-03 19:11:19 -0700

    Fix text rule splitting text into characters too extremely

--- src/rules/text.ts
+++ src/rules/text.ts
@@ -2,14 +2,15 @@ import SimpleMarkdown from 'simple-markdown';
 import { SimpleMarkdownRule } from './ruleType.js';
 import { urlRegex } from './url.js';
 
-// Modifies original text rule to not output backslashes from urls
+// Modifies original text rule html to not output backslashes from urls
 // Also modifies the match function to not only capture text before `/`
 // This is used for matching redditlinks like `r/programing` so the `r`
 // is not captured before the rest of the link
-// Modifies match function to not capture text around www.
+// Modifies match function to not capture a number before a . (e.g. 123.) for list markers
+// Modifies match function to not capture www.
 export const text: SimpleMarkdownRule = Object.assign({}, SimpleMarkdown.defaultRules.text, {
 	match: SimpleMarkdown.anyScopeRegex(
-		/^[\s\S]+?(?=[^0-9A-Za-z\s\u00c0-\uffff]|\n\n| {2,}\n|\w+(?::|\/|www.)|\S|$)/
+		/^[\s\S]+?(?=[^0-9A-Za-z\s\u00c0-\uffff]|\n\n| {2,}\n|\w+:\S|r\/|\d+\.|www\.|$)/
 	) satisfies SimpleMarkdown.MatchFunction,
 	html: function (node) {
 		if (node.content.match(urlRegex)) {
