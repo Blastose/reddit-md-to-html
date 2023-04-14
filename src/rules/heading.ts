1c0967534e51c3bf98576f9aed157e3c542d400e
Blastose <blastoisejay@gmail.com>
2023-04-13 20:09:46 -0700

    Fix heading with spaces in front (#17)

--- src/rules/heading.ts
+++ src/rules/heading.ts
@@ -3,5 +3,5 @@ import { SimpleMarkdownRule } from './ruleType.js';
 
 // Modifies original heading rule to support a new block on the next line
 export const heading: SimpleMarkdownRule = Object.assign({}, SimpleMarkdown.defaultRules.heading, {
-	match: SimpleMarkdown.blockRegex(/^ *(#{1,6}) *([^\n]+?) *#* *(?:\n *)*\n/)
+	match: SimpleMarkdown.blockRegex(/^ {0,3}(#{1,6}) *([^\n]+?) *#* *(?:\n *)*\n/)
 });
