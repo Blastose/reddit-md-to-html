2ddf763035211f7d5b53bd7f6ef568b7d934b409
Blastose <blastoisejay@gmail.com>
2023-04-25 10:12:58 -0700

    Fix list skipping when space at the end of line

--- tests/unorderedlist.test.ts
+++ tests/unorderedlist.test.ts
@@ -63,6 +63,21 @@ describe('unorderedlist', () => {
 			'<ul><li><p>Playin Stage</p><ul><li>8 teams participate</li><li>Teams are drawn into two double elimination brackets</li><li>Bracket matches are best of three</li><li>LCQ match is best of five</li><li>Winners of each group and the winner of the LCQ match between the lower bracket winners advance to Stage 2</li></ul></li><li><p>Bracket Stage</p><ul><li>8 teams participate</li><li>Double elimination bracket</li><li>All matches are Best of 5</li><li>Teams are drawn into the bracket with teams from tier 1 and 2 playing teams from tier 3.</li><li>Teams from tier 1 are placed on the opposite side of the bracket.</li><li>Teams from the same region can&#x27;t be draw in the first match of the bracket stage.</li></ul></li></ul><hr><h3>Tiers</h3>'
 		);
 	});
+
+	test('unordered list with text before and a space at the end of that text', () => {
+		const text = `# NEW BANNERS
+
+During this event, there will be **TWO DIFFERENT BANNERS.** 
+
+* One of them will include all ships of the collab, but only the **new ones will have a higher chance of dropping**
+* One of rhem will consist exclusively of characters from the original run
+
+# New skins`;
+		const htmlResult = converter(text);
+		expect(htmlResult).toBe(
+			'<h1>NEW BANNERS</h1><p>During this event, there will be <strong>TWO DIFFERENT BANNERS.</strong> </p><ul><li>One of them will include all ships of the collab, but only the <strong>new ones will have a higher chance of dropping</strong></li><li>One of rhem will consist exclusively of characters from the original run</li></ul><h1>New skins</h1>'
+		);
+	});
 });
 
 describe('nested unorderedlists', () => {
