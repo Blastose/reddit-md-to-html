58fd5dd1bd8ddbfcd7a828f8db87e0df8315f866
Blastose <blastoisejay@gmail.com>
2023-05-03 10:24:54 -0700

    Fix link rule to match when space between [ ] and ( )

--- src/rules/link.ts
+++ src/rules/link.ts
@@ -9,7 +9,7 @@ const LINK_HREF_AND_TITLE =
 	'\\s*<?((?:\\([^)]*\\)|[^\\\\]|\\\\.)*?)>?(?:\\s+[\'"(]([\\s\\S]*?)[\'")])?\\s*';
 
 // Link regex for easy copy pasting
-// ^\[((?:\[[^\]]*\]|[^[\]]|\](?=[^[]*\]))*)\]\(\s*<?((?:\([^)]*\)|[^\\]|\\.)*?)>?(?:\s+['"(]([\s\S]*?)['")])?\s*\)
+// ^\[((?:\[[^\]]*\]|[^[\]]|\](?=[^[]*\]))*)\] *\(\s*<?((?:\([^)]*\)|[^\\]|\\.)*?)>?(?:\s+['"(]([\s\S]*?)['")])?\s*\)
 
 // Modifies original link rule to add a state when parsing to tell other rules
 // that the text has already been ran through the link rule
@@ -18,7 +18,7 @@ const LINK_HREF_AND_TITLE =
 // Also outputs prepends https:// to of www. links
 export const link: SimpleMarkdownRule = Object.assign({}, SimpleMarkdown.defaultRules.link, {
 	match: SimpleMarkdown.inlineRegex(
-		new RegExp('^\\[(' + LINK_INSIDE + ')\\]\\(' + LINK_HREF_AND_TITLE + '\\)')
+		new RegExp('^\\[(' + LINK_INSIDE + ')\\] *\\(' + LINK_HREF_AND_TITLE + '\\)')
 	) satisfies SimpleMarkdown.MatchFunction,
 	parse: function (capture, parse, state) {
 		state.link = true;
